7ee154936306f960e685fe9c0b6e98cee3fa8e09
Yuliia Hubska <zhukapuka10@gmail.com>
2025-03-07 21:44:52 +0200

    Task5 done

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+//Напиши скрипт, який змінює колір фону елемента <body>
+// через інлайн-стиль по кліку на button.change-color
+// і задає це значення кольору текстовим вмістом для span.color.
+
+const colorChangeBtn = document.querySelector(".change-color");
+
+colorChangeBtn.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  document.querySelector("body").style.backgroundColor = newColor;
+  document.querySelector(".color").textContent = newColor;
+});
