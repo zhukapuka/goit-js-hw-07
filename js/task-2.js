d51ac59d748ab96b7cedd51ba627c3a1e8b2bf80
Yuliia Hubska <zhukapuka10@gmail.com>
2025-03-07 21:22:09 +0200

    Taks2 done

--- js/task-2.js
+++ js/task-2.js
@@ -22,5 +22,15 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
+const listElem = document.querySelector(".gallery");
+
+const galleryMarkUp = images
+  .map(
+    (image) =>
+      `<li class="gallery-item"><img class="item-image" src="${image.url}" alt="${image.alt}"></li>`
+  )
+  .join("");
+
+listElem.insertAdjacentHTML("afterbegin", galleryMarkUp);
